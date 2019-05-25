ffc026f6ff73db94b48b6b50212263170ca2bf85
刘志远 <dayuange12@sina.com>
2019-05-25 23:03:41 +0800

    update

--- src/from0to100/UniquePaths.java
+++ src/from0to100/UniquePaths.java
@@ -1,15 +1,20 @@
 package from0to100;
 
+/**
+ *
+ *    动态规划
+ *     每个节点到终点的路径等于 他右边和下边的之和
+ *     动态规划
+ *     从 m-1 n-1   m代表列 ，n代表行
+ *     cout为 每个位置到终点的路径书
+ *     如果 n=边界-1，cout(n,m)= (n,m+1)
+ *     如果 m=边界-1， cout=(n+1,m)
+ *     其他位置 等于 (n,m+1)+(n+1,m)
+ *     count(m-1,n-1)=1
+ */
 public class UniquePaths {
 
-    //每个节点到终点的路径等于 他右边和下边的之和
-    //动态规划
-    //从 m-1 n-1   m代表列 ，n代表行
-    //cout为 每个位置到终点的路径书
-    //如果 n=边界-1，cout(n,m)=   (n,m+1)
-    //如果 m=边界-1， cout=  (n+1,m)
-    //其他位置 等于 (n,m+1)+(n+1,m)
-    //count(m-1,n-1)=1
+
     public int uniquePaths(int m, int n) {
         int[][] s=new int[n][m];
         s[n-1][m-1]=1;
